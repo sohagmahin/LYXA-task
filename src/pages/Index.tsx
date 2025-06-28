dc58e1ac75e7b593d662d425b40967e40ee19679
sohagmahin <sohagmahin@gmail.com>
2025-06-28 16:36:47 +0600

    style: fix app width issue

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,11 @@
 import TodoBoard from "../components/TodoBoard";
 
 const Index = () => {
-  return <TodoBoard />;
+  return (
+    <div className="bg-amber-100 w-screen">
+      <TodoBoard />
+    </div>
+  );
 };
 
 export default Index;
